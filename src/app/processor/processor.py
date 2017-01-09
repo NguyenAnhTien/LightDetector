d612fcd448c33f68f3b9d69cac274b07438eecf0
NguyenAnhTien <tien.nguyenanh94@gmail.com>
2017-01-09 22:29:10 +0700

    TienNguyen: UPDATE processor class.

--- src/app/processor/processor.py
+++ src/app/processor/processor.py
@@ -9,23 +9,61 @@ Modified History
 2016-Dec-19 Created tien.nguyenanh94@gmail.com
 """
 
+import sys
+import threading
 import zmq
+
+import optionParser
 
 from cffi_interfaces.__cffi_jsonCommon import jsonCommon_c
 from cffi_interfaces.__cffi_jsonCommon import jsonCommon_cffi
 
-# ZeroMQ Context
-context = zmq.Context()
+class Processor(threading.Thread):
+    def __init__(self, host, port, topic=None):
+        threading.Thread.__init__(self)
+        self.host = host 
+        self.port = port
+        self.topic = topic
+        self.context = zmq.Context()
+        self.sock = self.context.socket(zmq.SUB)
 
-# Define the socket using the "Context"
-sock = context.socket(zmq.SUB)
+        if topic != None:
+            for item in self.topic:
+                self.sock.setsockopt_string(zmq.SUBSCRIBE, item)
 
-# Define subscription and messages with prefix to accept.
-sock.setsockopt_string(zmq.SUBSCRIBE, "LIGHT_INTENSITY_TOPIC")
-sock.connect("tcp://127.0.0.1:5563")
+    def connect(self):
+        cmd = "tcp://" + self.host
+        cmd = cmd + ":"
+        cmd = cmd + self.port
+        self.sock.connect(cmd)
 
-while True:
-    message= sock.recv()
-    print("Message: ", message)
-    # topic, messagedata = message.split()
-    # print("Topic: %s Message: %s" %(topic, message))
+    def subscribeTopic(self, topic):
+
+        if self.topic == None:
+            self.topic = topic
+        else:    
+            self.topic + topic
+
+        for item in topic:
+            self.sock.setsockopt_string(zmq.SUBSCRIBE, item)            
+
+    def run(self):
+        print("Processor run on %s:%s" %(self.host, self.port))
+        message = self.sock.recv()
+        print("Message: ", message)
+
+if __name__ == '__main__':
+
+    if len(sys.argv) < 4:
+        sys.exit(2)
+
+    host, port, topic = optionParser.parseCmdLineArg(sys.argv)    
+
+    processor = Processor(host, port, topic)
+    processor.connect()
+    
+    try:
+        processor.run()
+    except KeyboardInterrupt:
+        print("Keyboard Interruption Errors.")
+
